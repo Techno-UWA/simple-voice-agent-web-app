62137983dd3035dd425c73dde39aaad87ea149c8
Techno-UWA <117506201+Techno-UWA@users.noreply.github.com>
2025-06-21 22:04:05 +0000

    Fixed config file by moving pwa stuff into withpwa instead of nextConfig

--- voice-agent-app/next.config.ts
+++ voice-agent-app/next.config.ts
@@ -1,13 +1,12 @@
 import type { NextConfig } from "next";
-const withPWA = require('next-pwa');
+const withPWA = require('next-pwa')({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-  },
+  // any other Next.js config options
 };
 
 export default withPWA(nextConfig);
